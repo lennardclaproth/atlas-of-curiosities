a452521b5604030b69b40f52678f98cb7e86e98a
lennardclaproth <lennardclaproth@gmail.com>
2025-10-25 23:42:14 +0200

    comments on stack

--- playground-golang/algorithms/internal/data-structures/stack.go
+++ playground-golang/algorithms/internal/data-structures/stack.go
@@ -4,10 +4,15 @@ type Stack[T any] struct {
 	data []T
 }
 
+// push adds a new item to the stack data structure. It does this by
+// appending it to the end of the data of the stack.
 func (s *Stack[T]) Push(d T) {
 	s.data = append(s.data, d)
 }
 
+// Pop gets the last added item of the stack and returns it.
+// the stack data structure rests on the LIFO principle (Last
+// In First Out)
 func (s *Stack[T]) Pop() T {
 	d := s.data[len(s.data)-1]
 	s.data = s.data[:len(s.data)-1]
